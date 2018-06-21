450e9f78f009e5f97b9a85d1275f74c3dbe2ed14
Andrew Dicer <andrew.dicer@gmail.com>
2018-06-21 18:08:42 -0500

    updated scrape

--- server.js
+++ server.js
@@ -16,37 +16,51 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-let data =  [{title: "Avengers: Infinity War", amount: "$664,987,816"}]
- 
-request("http://www.boxofficemojo.com/seasonal/?view=releasedate&yr=2018&season=Summer", function(error, response, html) {
+let data =  []
 
+request("http://www.boxofficemojo.com/seasonal/?chart=&season=Spring&yr=2018&view=releasedate", function(error, response, html) {
   const $ = cheerio.load(html);
-
   $('div#body').children('center').children('table').children('tbody').children().each(function(i, element) {
-    // Save the data in variables 
-    if(i < 10 && i > 0) {
+
+    if(i < 2 && i > 0) {
+      
     const title = $(element).children().eq(1).text();
     const amt = $(element).children().eq(3).text();
-
     let movieInfo = { title: title, amount: amt}
     data.push(movieInfo)
     }
   })
-
-  players.forEach(function(player){
-    player.picks.forEach(function(pick, i){
-      data.forEach(function(title, a){
-        if(pick === title.title && i < 10){
-          if(a === i) player.points += 10;
-          else player.points += (8 - Math.abs(i-a));
-        }
-        else if(pick === title.title )player.points += 1;
-      })     
-    })
-  })
-
-  players.sort(function compareNumbers(a, b){return b.points - a.points})
+  summer();
 })
+ 
+summer = () => {
+  request("http://www.boxofficemojo.com/seasonal/?view=releasedate&yr=2018&season=Summer", function(error, response, html) {
+    const $ = cheerio.load(html);
+    $('div#body').children('center').children('table').children('tbody').children().each(function(i, element) {
+      
+      if(i < 10 && i > 0) {
+      const title = $(element).children().eq(1).text();
+      const amt = $(element).children().eq(3).text();
+      let movieInfo = { title: title, amount: amt}
+      data.push(movieInfo)
+      }
+    })
+
+    players.forEach(function(player){
+      player.picks.forEach(function(pick, i){
+        data.forEach(function(title, a){
+          if(pick === title.title && i < 10){
+            if(a === i) player.points += 10;
+            else player.points += (8 - Math.abs(i-a));
+          }
+          else if(pick === title.title )player.points += 1;
+        })     
+      })
+    })
+
+    players.sort(function compareNumbers(a, b){return b.points - a.points})
+  })
+}
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
