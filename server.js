2c96ac95486e1aae2e76ff2945ce0caa765afe7d
Andrew Dicer <andrew.dicer@gmail.com>
2018-06-25 16:06:02 -0500

    fixed async

--- server.js
+++ server.js
@@ -23,12 +23,6 @@ boxofficeBuild = () => {
   data = []
 
   boxofficeScrape("http://www.boxofficemojo.com/seasonal/?chart=&season=Spring&yr=2018&view=releasedate", 2);
-  setTimeout(() => { boxofficeScrape("http://www.boxofficemojo.com/seasonal/?view=releasedate&yr=2018&season=Summer", 10)}, 500);
- 
-  setTimeout(() => {
-    playerBuild(all, "all")
-    playerBuild(gurus, "gurus")
-  }, 700);
 }
 
 playerBuild = (group, type) => {
@@ -115,15 +109,20 @@ playerBuild = (group, type) => {
 
 boxofficeScrape = (url, number) => {
   request(url, function(error, response, html) {
-      const $ = cheerio.load(html);
-      $('div#body').children('center').children('table').children('tbody').children().each(function(i, element) {  
-          if(i < number && i > 0) {
-          const title = $(element).children().eq(1).text();
-          const amt = $(element).children().eq(3).text();
-          let movieInfo = { title: title, amount: amt}
-          data.push(movieInfo)
-          }
-      })
+    const $ = cheerio.load(html);
+    $('div#body').children('center').children('table').children('tbody').children().each(function(i, element) {  
+      if(i < number && i > 0) {
+      const title = $(element).children().eq(1).text();
+      const amt = $(element).children().eq(3).text();
+      let movieInfo = { title: title, amount: amt }
+      data.push(movieInfo)
+      }
+    })
+    if(number === 2)boxofficeScrape("http://www.boxofficemojo.com/seasonal/?view=releasedate&yr=2018&season=Summer", 10);
+    else {   
+        playerBuild(all, "all")
+        playerBuild(gurus, "gurus")
+    }
   })
 }
 
