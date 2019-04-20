7e5d3d590683d136c7349882a00bdb3e7c343f5f
Andrew Dicer <andrew.dicer@gmail.com>
2019-04-19 19:14:17 -0500

    '2019'

--- public/assets/javascript/form.js
+++ public/assets/javascript/form.js
@@ -81,10 +81,15 @@ $(".nameSubmit").on('click', function(event){
     
 })
 
-let mov1; let mov2;
 //Summer Movie List ------------------------------------------------------
 $.ajax({ url: "https://summersum.herokuapp.com/api/list", method: "GET"})
-.done((res) => console.log(res.ruslts));
+.done((res) => { list = res.results.sort(function(a, b){
+    const mov1 = a.title.toLowerCase(), mov2 = b.title.toLowerCase()
+    if (mov1 < mov2) return -1 
+    if (mov1 > mov2) return 1
+    return 0 
+    }), listOptions() 
+});
 
 //Input Change --------------------------------------------------------
 $(".list").on('change',  function(event) {
