9edf2c2e894af3df71264a1c7ae3983931926134
Andrew Dicer <andrew.dicer@gmail.com>
2019-04-20 20:15:41 -0500

    '2019'

--- public/assets/javascript/data.js
+++ public/assets/javascript/data.js
@@ -1,6 +1,7 @@
 $.ajax({ url: "/api/users", method: "GET"})
         .done((res) => { 
+            const data = res.results.sort(function(a, b){return a.guru - b.guru})
             res.results.map((user, i)=> {
-                $("#data").append("<div>" + i + " - Name: " + user.username + " / guru: " + user.guru + " / twitter: " + user.twitter + "<hr/></div>")      
+                $("#data").append("<div>" + (i+1) + " - Name: " + user.username + " / guru: " + user.guru + " / twitter: " + user.twitter + "<hr/></div>")      
             })
         });
