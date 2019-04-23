fd9ed3f22a2af78a4d7a4515601eba08aeee3f7f
Andrew Dicer <andrew.dicer@gmail.com>
2019-04-23 11:05:51 -0500

    '2019'

--- public/assets/javascript/data.js
+++ public/assets/javascript/data.js
@@ -1,18 +1,16 @@
 $.ajax({ url: "/api/users", method: "GET"})
-        .done((res) => { 
-            res.results.sort(function(a, b){return b.guru - a.guru})
-            res.results.map((user, i)=> {
-                
-                let guru = ""; let twitter = "";
-                user.guru ? guru = "<i class='far fa-check-circle up'></i>" : guru = "<i class='far fa-times-circle down'></i>"
-                user.twitter ? twitter = "<i class='far fa-check-circle up'></i>" : tiwtter = "<i class='far fa-times-circle down'></i>"
+    .done((res) => { 
+        res.results.sort(function(a, b){return b.guru - a.guru})
+        res.results.map((user, i)=> {
+            
+            let guru = ""; let twitter = "";
+            user.guru ? guru = "<i class='far fa-check-circle up'></i>" : guru = "<i class='far fa-times-circle down'></i>"
+            user.twitter ? twitter = "<i class='far fa-check-circle up'></i>" : twitter = "<i class='far fa-times-circle down'></i>"
 
-                $("#data").append("<div>" + (i+1) + ": " + user.username + 
-                " / <i class='fas fa-user-astronaut guru guru-check'></i>  " + guru + 
-                " / <i class='fab fa-twitter'></i> " + twitter + "<hr/></div>")      
-            })
-        });
+            $("#data").append("<div>" + (i+1) + ": " + user.username + 
+            " - <i class='fas fa-user-astronaut guru '></i>  " + guru + 
+            " - <i class='fab fa-twitter'></i> " + twitter + "<hr/></div>")      
+        })
+    });
 
-$(".guru-check").click((event)=>{
-    console.log('guru click');
-})
+    
