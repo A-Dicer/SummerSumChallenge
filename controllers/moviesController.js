eb842d949940ab50c0bc1f20b6d2e74f98f7c2d6
Andrew Dicer <andrew.dicer@gmail.com>
2019-07-08 15:19:46 -0500

    mid changes

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -30,21 +30,41 @@ module.exports = {
   //-------------------------- Check Movies ------------------------------
   check: function(req, res) {
     let data = [];
-
+    console.log("part1")
     db.Movies.find(req.query)
         .then(dbModel => {
           boxofficeScrape("http://www.boxofficemojo.com/seasonal/?chart=&season=Spring&yr=2019&view=releasedate", 2, dbModel)
         })
         .catch(err => res.status(422).json(err));
 
+
+    boxofficeScrape = (url, number, oldInfo) => {
+      console.log("part2")
+      request(url, function(error, response, html) {
+        const $ = cheerio.load(html);
+        $('div#body').children('center').children('table').children('tbody').children().each(function(i, element) {  
+          if(i < number && i > 0) {
+          const title = $(element).children().eq(1).text();
+          const amt = $(element).children().eq(3).text();
+          let movieInfo = { title: title, amount: amt }
+          data.push(movieInfo)
+          }
+        })
+        
+        number === 2
+        ? boxofficeScrape("http://www.boxofficemojo.com/seasonal/?view=releasedate&yr=2019&season=Summer", 10, oldInfo)
+        : finish(oldInfo, data)
+      })
+    }
+
     finish = (oldInfo, newInfo) => {
-      // console.log(oldInfo[oldInfo.length-1])
+      console.log("part3")
 
       oldInfo.length && newInfo.length 
         ? (
             //set oldMovies to the most recent list
             oldMovie = JSON.parse(JSON.stringify(oldInfo[oldInfo.length- 1].movies)),
-            
+            console.log(oldMovie[0].title),
             //scrape new list from boxOffice mojo
                 //make sure scrape worked
                 newInfo.length
@@ -68,24 +88,6 @@ module.exports = {
         : null
     }
 
-    boxofficeScrape = (url, number, oldInfo) => {
-      // console.log(oldInfo[oldInfo.length-1])
-      request(url, function(error, response, html) {
-        const $ = cheerio.load(html);
-        $('div#body').children('center').children('table').children('tbody').children().each(function(i, element) {  
-          if(i < number && i > 0) {
-          const title = $(element).children().eq(1).text();
-          const amt = $(element).children().eq(3).text();
-          let movieInfo = { title: title, amount: amt }
-          data.push(movieInfo)
-          }
-        })
-        
-        number === 2
-        ? boxofficeScrape("http://www.boxofficemojo.com/seasonal/?view=releasedate&yr=2019&season=Summer", 10, oldInfo)
-        : finish(oldInfo, data)
-      })
-    }
   },
   
 
